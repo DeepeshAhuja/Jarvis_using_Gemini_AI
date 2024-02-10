8558dbd6c450346d4cc910ccb53602abdc45fdff
DeepeshAhuja <deepesh.ahuja002@gmail.com>
2024-02-10 23:04:11 +0530

    getting info from gemini ai thru api

---
+++ Gemini_content1.py
@@ -0,0 +1,22 @@
+from config import key
+import requests #web
+
+
+
+def chat1(chat):
+    messages = [] #list with all messages
+    system_message = "You are an AI bot, your name is Jarvis. " # first instruction
+    message = {"role" : "user", "parts" : [{"text": system_message+" "+chat}]}
+    messages.append(message)
+    data = {"contents" : messages}
+    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key="+key
+    response = requests.post(url, json=data)
+    
+    t1 = response.json()
+    # print(t1)
+    t2 = t1.get("candidates")[0].get("content").get("parts")[0].get("text")
+    print(t2)
+    
+chat = input("Enter your message: ")
+# chat="who is Deepesh Ahuja"
+chat1(chat)
