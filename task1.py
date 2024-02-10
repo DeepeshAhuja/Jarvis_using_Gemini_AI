32dc4666dfb6265fcad7f5135270e4414dbe58aa
DeepeshAhuja <deepesh.ahuja002@gmail.com>
2024-02-10 23:07:19 +0530

    function to get humidity and temperature of any state thru weather api

---
+++ task1.py
@@ -0,0 +1,23 @@
+import requests
+
+def temp_city(city):
+    url = "https://yahoo-weather5.p.rapidapi.com/weather"
+
+    querystring = {"location":city,"format":"json","u":"f"}
+
+    headers = {
+        "X-RapidAPI-Key": "a88504aef0msh3d8773426b85d0fp1a5345jsnafbfd29d5530",
+        "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    d1 = response.json()
+    d1 = d1.get("current_observation")
+    hum = d1.get("atmosphere").get("humidity")
+    temp = d1.get("condition").get("temperature")
+    temp = round((temp-32)*5/9,2)
+    return (f"humidity:  {hum}, Temperature: {temp}")
+
+if __name__ == "__main__":
+    temp_city("Hyderabad")
