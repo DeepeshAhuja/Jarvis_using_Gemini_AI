d2d88b971b00613983067ee11f4d264e6d14f417
DeepeshAhuja <deepesh.ahuja002@gmail.com>
2024-02-11 23:36:26 +0530

    added description for task

--- task1.py
+++ task1.py
@@ -1,6 +1,10 @@
 import requests
 from config import headers
 
+def room_temp(room):
+    result = "Temperature = 20, Humidity = 50"
+    return result
+
 def temp_city(city):
     url = "https://yahoo-weather5.p.rapidapi.com/weather"
 
@@ -15,5 +19,36 @@ def temp_city(city):
     temp = round((temp-32)*5/9,2)
     return (f"humidity:  {hum}, Temperature: {temp}")
 
+definations = [
+    {
+        "name":"temp_city",  # name of the function to be called
+        "description": "find weather, temperature of a city",
+        "parameters":
+            {
+                "type":"object",
+                "properties":{
+                    "city" : {                 # Argument for function temp_city
+                        "type":"string",
+                        "description":"city to find weather"
+                    }
+                }
+            }
+    },
+    {
+        "name":"temp_room",  # name of the function to be called
+        "description": "find temperature of my room or my home",
+        "parameters":
+            {
+                "type":"object",
+                "properties":{
+                    "room" : {                 # Argument for function temp_city
+                        "type":"string",
+                        "description":"room or home"
+                    }
+                }
+            }
+    }
+]
+
 if __name__ == "__main__":
-    temp_city("Hyderabad")
+    print(temp_city("Hyderabad"))
