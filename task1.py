dac534521614ecf9783dcab63aa7d16ae705a489
DeepeshAhuja <deepesh.ahuja002@gmail.com>
2024-02-11 21:19:15 +0530

    updated weather api

--- task1.py
+++ task1.py
@@ -1,14 +1,10 @@
 import requests
+from config import headers
 
 def temp_city(city):
     url = "https://yahoo-weather5.p.rapidapi.com/weather"
 
     querystring = {"location":city,"format":"json","u":"f"}
-
-    headers = {
-        "X-RapidAPI-Key": "a88504aef0msh3d8773426b85d0fp1a5345jsnafbfd29d5530",
-        "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com"
-    }
 
     response = requests.get(url, headers=headers, params=querystring)
 
