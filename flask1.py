edffa80f29434ae99982c5fe61d6be5638cd751d
DeepeshAhuja <deepesh.ahuja002@gmail.com>
2024-02-13 23:58:44 +0530

    completed basic website

---
+++ flask1.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, jsonify, request
+from agent1 import run_conversation
+
+app = Flask(__name__)
+
+@app.route('/process_message', methods=['POST'])
+def process_message_func1():
+    msg=request.json['message']
+    print(msg)
+    resp = run_conversation(msg)
+    return jsonify({"response": resp })
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000,debug=True)
