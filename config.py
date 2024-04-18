e6675c9be850faa941886283c8407dad56e0ef26
Deepesh Ahuja <deepesh.ahuja002@gmail.com>
2024-04-18 14:00:02 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-key = "AIzaSyCorFyOkM7g2YlyjGrjWUaLtE2lyKrKdDI"
-
-headers = {
-        "X-RapidAPI-Key": "a88504aef0msh3d8773426b85d0fp1a5345jsnafbfd29d5530",
-        "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com"
-    }
