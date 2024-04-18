5f0b1fb326d3f82ccfbacf8a025d569e411ed303
DeepeshAhuja <deepesh.ahuja002@gmail.com>
2024-04-18 14:01:53 +0530

    config file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-key = "AIzaSyCorFyOkM7g2YlyjGrjWUaLtE2lyKrKdDI"
+key = "<your gemini key>"
 
 headers = {
-        "X-RapidAPI-Key": "a88504aef0msh3d8773426b85d0fp1a5345jsnafbfd29d5530",
+        "X-RapidAPI-Key": "<rapidapi weather key",
         "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com"
     }
