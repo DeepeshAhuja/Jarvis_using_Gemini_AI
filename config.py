56e8567919dbd0a9423876a03c018d6aaf801b9f
DeepeshAhuja <deepesh.ahuja002@gmail.com>
2024-04-18 14:04:32 +0530

    updated config file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 key = "<your gemini key>"
 
 headers = {
-        "X-RapidAPI-Key": "<rapidapi weather key",
+        "X-RapidAPI-Key": "<your rapidapi weather key",
         "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com"
     }
