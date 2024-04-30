469babff100ee9209baa207b259dd1b2a11e28cd
DeepeshAhuja <deepesh.ahuja002@gmail.com>
2024-04-30 20:50:49 +0530

    updated config file

--- config.py
+++ config.py
@@ -1,6 +1,17 @@
 key = "<your gemini key>"
 
-headers = {
+weather = {
         "X-RapidAPI-Key": "<your rapidapi weather key>",
         "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com"
+    }
+
+mail = {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "<your rapidapi mail key>",
+        "X-RapidAPI-Host": "mail-sender-api1.p.rapidapi.com"
+    }
+
+news = {
+        "X-RapidAPI-Key": "<your rapidapi news key>",
+        "X-RapidAPI-Host": "real-time-news-data.p.rapidapi.com"
     }
